f0d792e5a5877092c9afe721ac1906737b8a3edf
otori23 <azeez.otori@gmail.com>
2015-11-17 23:27:15 -0800

    add knockoutjs and create initial app viewModel

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
-(function() {
-	
+(function() {	
+	function YottaCycleAppViewModel() {
+		// Data
+	    var self = this;
+	    self.places = ko.observableArray([]);
+	    self.filteredPlaces = ko.computed(function() {
+	        return ko.utils.arrayFilter(self.places(), function(place) { return true; });
+	    });
+
+	    // Operations
+	    self.onHamburgerClick = function() {
+      		var placesSection = document.querySelector(".places-section");
+      		placesSection.classList.toggle('open-places-section');
+	    };
+
+	    self.onSearchBarClick = function() {
+      		var textBox = document.querySelector('input[type="text"]');
+        	textBox.focus();
+        };
+	}
+
+	ko.applyBindings(new YottaCycleAppViewModel());
 })();
