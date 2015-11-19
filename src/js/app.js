31b42bdcd4e38dc9ede52e380abe809fbc755fa5
otori23 <azeez.otori@gmail.com>
2015-11-19 00:48:44 -0800

    build data model with ajax request and bind model to list of places

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 	    var self = this;
 	    self.places = ko.observableArray([]);
 	    self.filteredPlaces = ko.computed(function() {
-	        return ko.utils.arrayFilter(self.places(), function(place) { return true; });
+	        return ko.utils.arrayFilter(self.places(), function(place) { return !place._destroy; });
 	    });
 
 	    // Event Handlers
@@ -29,6 +29,19 @@
         });
 
         // Load Data (From 4Square or LocalStorage)
+        $.ajax({
+  			url: "https://api.foursquare.com/v2/venues/search?client_id=XOVHNWG4KKKESGADD0HOE3SWYTXVWYAPWHSQFC4CO4FHE4R5&client_secret=FHXKIUQOFHCJUXQYECAVH3DYE50JEJZ1N1AKONXHHEMWVLZR&v=20130815&ll=37.338141,-121.886366&query=recycle",
+
+  			success: function(data, textStatus, jqXHR) {
+  				var venues = $.map(data.response.venues, function(venue) { venue._destroy = false; return venue; });
+  				self.places(venues);
+  			},
+
+  			error: function(jqXHR, textStatus, errorThrown) {
+  				// Do something sensible here
+  				alert("Error Loading Recycling Data");
+  			}
+		});
 
         // Draw Markers on Map
 	}
